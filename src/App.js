f459cf81cd4b3b7fc9ebaa2fecd05ec1a912c9e3
Paolo Landeros <landerospaolo@gmail.com>
2023-03-15 20:11:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./view/Home";
+import Pokemones from "./view/Pokemones"
+import PokeInfo from "./view/PokeInfo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/pokemones" element={<Pokemones />}/>
+          <Route path="/pokemones/:pokeSelect" element={<PokeInfo />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
